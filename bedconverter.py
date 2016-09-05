e500bb9b18a1bcc81215e7fffbffa5238a6d3513
Min Yi <mjaeyi@gmail.com>
2016-09-04 21:10:36 -0700

    Creation of bedconverter.py to convert files to bed format

---
+++ bedconverter.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+import sys
+import getopt
+
+inputfile = ''
+outputfile = ''
+opts, args = getopt.getopt(sys.argv[1:],"i:o:")
+
+for opt, arg in opts:
+	if opt == '-i':
+		inputfile = arg
+	elif opt == '-o':
+  	        outputfile = arg
+	else:
+   	        print "Usage: ./bedconverter.py -i filename -o filename"
+
+
+o = open(outputfile, 'w')
+
+with open(inputfile) as f:
+	for line in f:
+		gene = line
+		aGene = gene.split()
+
+		start = aGene[9]
+		end = aGene[10]
+
+		aStart = start.split(',')
+		aEnd = end.split(',')
+
+
+		for l in range(len(aGene)):
+			o.write(aGene[2] + "\t" + aStart[l] + "\t" + aEnd[l] + "\t" + aGene[1] + ':' + aGene[12] + "\t" + "NA" + "\t" + aGene[3])
+			o.write("\n") 
+
+
+f.close()
+o.close()
+
+
