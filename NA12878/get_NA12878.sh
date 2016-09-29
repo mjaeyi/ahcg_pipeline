c4114c33ff9a521c9ce96c4f6cf6f3e8cfec86f3
Min Yi <mjaeyi@gmail.com>
2016-09-29 13:44:28 -0700

    NA12878 fq files and script

---
+++ NA12878/get_NA12878.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+wget ftp://ftp-trace.ncbi.nih.gov/giab/ftp/data/NA12878/Garvan_NA12878_HG001_HiSeq_Exome/NIST7035_TAAGGCGA_L001_R1_001.fastq.gz
+wget ftp://ftp-trace.ncbi.nih.gov/giab/ftp/data/NA12878/Garvan_NA12878_HG001_HiSeq_Exome/NIST7035_TAAGGCGA_L001_R2_001.fastq.gz
+wget ftp://ftp-trace.ncbi.nih.gov/giab/ftp/data/NA12878/Garvan_NA12878_HG001_HiSeq_Exome/NIST7035_TAAGGCGA_L002_R1_001.fastq.gz
+wget ftp://ftp-trace.ncbi.nih.gov/giab/ftp/data/NA12878/Garvan_NA12878_HG001_HiSeq_Exome/NIST7035_TAAGGCGA_L002_R2_001.fastq.gz
+wget ftp://ftp-trace.ncbi.nih.gov/giab/ftp/data/NA12878/Garvan_NA12878_HG001_HiSeq_Exome/NIST7086_CGTACTAG_L001_R1_001.fastq.gz
+wget ftp://ftp-trace.ncbi.nih.gov/giab/ftp/data/NA12878/Garvan_NA12878_HG001_HiSeq_Exome/NIST7086_CGTACTAG_L001_R2_001.fastq.gz
+wget ftp://ftp-trace.ncbi.nih.gov/giab/ftp/data/NA12878/Garvan_NA12878_HG001_HiSeq_Exome/NIST7086_CGTACTAG_L002_R1_001.fastq.gz
+wget ftp://ftp-trace.ncbi.nih.gov/giab/ftp/data/NA12878/Garvan_NA12878_HG001_HiSeq_Exome/NIST7086_CGTACTAG_L002_R2_001.fastq.gz
+
+md5sum -c NA12878_md5sum.txt | tee -a NA12878.log
+
+echo R1 >> NA12878.log
+zcat NIST70??_????????_L00?_R1_001.fastq.gz \
+	| tee >(wc -l >> NA12878.log) \
+	| gzip -9 > NA12878_final_1.fastq.gz 
+
+echo R2 >> NA12878.log
+zcat NIST70??_????????_L00?_R2_001.fastq.gz \
+	| tee >(wc -l >> NA12878.log) \
+	| gzip -9 > NA12878_final_2.fastq.gz
+
