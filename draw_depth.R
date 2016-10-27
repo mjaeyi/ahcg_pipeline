c84d72b4195a3a2b00f69d30555b15f34202dc8a
Min Yi <mjaeyi@gmail.com>
2016-10-27 07:02:23 -0700

    R script to plot depth

--- draw_depth.R
+++ draw_depth.R
@@ -9,7 +9,7 @@ fn=args[1]
 cov <- read.delim(fn, header=FALSE)
 names(cov) <- c("CHR", "POSITION", "COVERAGE")
 
-pdf(paste("coverage.pdf"))
+png(filename="coverage.pdf")
 ggplot(cov, aes(POSITION, COVERAGE)) + geom_point() + geom_hline(yintercept = 30, color = "green") +
 ggtitle(paste("Depth of Coverage"))
-
+dev.off()
